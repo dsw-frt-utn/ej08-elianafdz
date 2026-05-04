2246f57dc59dc3914152a0b8d9fb4d26d8de35ae
Eliana Fernandez <elianafernandez916@gmail.com>
2026-05-04 14:42:36 -0300

    Resuelvo problema 4

---
+++ Dsw2026Ej8/Problema4.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema4
+    {
+        public static double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            int suma = 0;
+            int cantidad = 0;
+
+            // nota 1
+            if (nota1.HasValue && nota1 > 0 && nota1 < 10)
+            {
+                suma += nota1.Value;
+                cantidad++;
+            }
+
+            // nota 2
+            if (nota2.HasValue && nota2 > 0 && nota2 < 10)
+            {
+                suma += nota2.Value;
+                cantidad++;
+            }
+
+            // nota 3
+            if (nota3.HasValue && nota3 > 0 && nota3 < 10)
+            {
+                suma += nota3.Value;
+                cantidad++;
+            }
+
+            if (cantidad == 0)
+            {
+                return 0;
+            }
+
+            return (double)suma / cantidad;
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -4,17 +4,8 @@
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello, World!");
-            Product p = new Product();
-            p.SetDescription("Original");
-
-            // llamamos al método
-            string resultado = Problema3.CompararCopias(5, p);
-
-            Console.WriteLine(resultado);
-
-            // vemos qué pasó con el producto original
-            Console.WriteLine(p.GetDescription());
+            // Console.WriteLine("Hello, World!");
+            Console.WriteLine(Problema4.CalcularPromedio(null, null, null));
         }
     }
 }
