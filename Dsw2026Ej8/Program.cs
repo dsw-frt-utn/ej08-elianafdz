295ee7fa2b92956202bae9fa842a37cb280e8ffd
Eliana Fernandez <elianafernandez916@gmail.com>
2026-05-04 04:38:15 -0300

    Resuelvo problema 2

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-          //  Console.WriteLine("Hello, World!");
-            Console.WriteLine(ProductHelper.ObtenerEtiquetaProducto(1, "Mouse", 1000));
+            //Console.WriteLine("Hello, World!");
+            Console.WriteLine(Problema2.CrearResumenVenta(1, "Mouse", 2, 1000));
         }
     }
 }
